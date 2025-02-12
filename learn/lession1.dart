1eedbbabc95edb6e5d79277c342b75d3ea941246
HoangTuan2112 <tuanmaiyeunguoido@gmail.com>
2025-02-12 14:45:41 +0700

    bien va khai bao bien

---
+++ learn/lession1.dart
@@ -0,0 +1,15 @@
+void main(){
+  String message = "Hello World";
+  print("Hello World");
+  print(message);
+
+  int x = 10;
+  double y = 20.0;
+  print(x) ;
+  print(y);
+
+  num z = 30;
+  print(z);
+
+
+}
