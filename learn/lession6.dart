22ccfe584c49777bafccac492b176b50895d09b0
HoangTuan2112 <tuanmaiyeunguoido@gmail.com>
2025-02-12 15:14:16 +0700

    for voi chi so

---
+++ learn/lession6.dart
@@ -0,0 +1,12 @@
+void main(){
+  var numbers=[1,2,3,4,5,6,7,8,9,10];
+  var fruits=['apple','banana','mango','orange','grapes'];
+
+  print('list of numbers');
+  for(var i=0;i<numbers.length;i++){
+    print('numbers[$i] = ${numbers[i]}');
+  }
+  print('list of fruits');
+
+
+}
