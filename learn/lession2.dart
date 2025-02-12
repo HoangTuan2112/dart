6f19c096bbdd185dfe9a9d1780c804002334c68f
HoangTuan2112 <tuanmaiyeunguoido@gmail.com>
2025-02-12 14:51:56 +0700

    cac toan tu so hoc

---
+++ learn/lession2.dart
@@ -0,0 +1,23 @@
+void main(){
+  var a =100;
+  var b =250;
+
+  print('a + b = ${a + b}');
+  print('a - b = ${a - b}');
+  print('a * b = ${a * b}');
+  print('a / b = ${a / b}');
+  print('a % b = ${a % b}');
+  print('a ~/ b = ${a ~/ b}');
+  print('a++ = ${a++}');
+  print('a-- = ${a--}');
+  print('++a = ${++a}');
+  print('--a = ${--a}');
+  print('a += b = ${a += b}');
+  print('a -= b = ${a -= b}');
+  print('a *= b = ${a *= b}');
+
+
+
+
+
+}
