d10295ca0c978ec548db595219c8fab1e88c9c9e
HoangTuan2112 <tuanmaiyeunguoido@gmail.com>
2025-02-12 15:19:33 +0700

    for in

---
+++ learn/lession7.dart
@@ -0,0 +1,13 @@
+void main() {
+  var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  var fruits = ['apple', 'banana', 'mango', 'orange', 'grapes'];
+
+  print('list of numbers');
+  for (var number in numbers) {
+    print(number);
+  }
+  print('list of fruits');
+  for (var fruit in fruits) {
+    print(fruit);
+  }
+}
