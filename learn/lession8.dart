655aa8851547d4eb913a6afd96cb549311dd0492
HoangTuan2112 <tuanmaiyeunguoido@gmail.com>
2025-02-12 15:27:31 +0700

    hang so final va const

---
+++ learn/lession8.dart
@@ -0,0 +1,11 @@
+void main() {
+  // constant in dart
+  final a = 100;
+  const b = 200;
+  // biến final chỉ có thể được gán một lần
+  // biến const là một hằng số tại thời điểm biên dịch
+  // biến final được khởi tạo khi được truy cập
+  // biến const được khởi tạo tại thời điểm biên dịch
+  // biến final có thể được khởi tạo tại thời gian chạy
+  // biến const phải được khởi tạo tại thời điểm biên dịch
+}
