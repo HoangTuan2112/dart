8b8daadd0c27fd7f8435da1afb9ea28e27fc32a0
HoangTuan2112 <tuanmaiyeunguoido@gmail.com>
2025-02-13 09:21:35 +0700

    tham so tuy chon va gia tri mac dinh cua tham so trong function

--- learn/lession15.dart
+++ learn/lession15.dart
@@ -3,6 +3,7 @@ void main(){
   goTo('Tuan', 'school', 'bike');
 
 }
+//optional parameter and default parameter
 void goTo(String who,String where,[String? by, String regulation = 'everyday']){
   print("$who go to $where ${by!=null?"by $by ":""}$regulation");
 }
